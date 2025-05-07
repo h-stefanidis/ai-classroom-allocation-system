5420c8761dcb65fafcd37d5a9f39eb5fcfb5f2a0
Alexander Blatchford <alexander.blatchford@gmail.com>
2025-05-07 16:44:04 +1000

    committing current changes to sql query functions

--- db/db_usage.py
+++ db/db_usage.py
@@ -1,5 +1,42 @@
 from db_manager import get_db
 
 db = get_db()
-results = db.fetch_all("SELECT * FROM raw.participants limit 5;")
-print(results)
+
+
+
+def get_all_participants():
+    with db:
+        df = db.query_df("SELECT participant_id FROM raw.participants;")
+        return df
+
+def create_allocations_table():
+    with db:
+        query = """
+        CREATE TABLE IF NOT EXISTS raw.allocations (
+            participant_id INTEGER PRIMARY KEY,
+            classroom_id INTEGER DEFAULT 0
+        );
+        """
+        db.execute_query(query)
+
+
+
+def populate_allocations_table():
+    participants_df = get_all_participants()
+    if participants_df is None or participants_df.empty:
+        print("No participants found.")
+        return
+
+    data = [(str(pid), 0) for pid in participants_df['participant_id']]
+
+    with db:
+        query = """
+        INSERT INTO raw.allocations (participant_id, classroom_id)
+        VALUES (%s, %s)
+        ON CONFLICT (participant_id) DO NOTHING;
+        """
+        db.execute_many(query, data)
+
+
+create_allocations_table()
+populate_allocations_table()

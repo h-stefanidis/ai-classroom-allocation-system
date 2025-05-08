3fdfc0fd8d41eec8421599de266c472ce82169a0
Alexander Blatchford <alexander.blatchford@gmail.com>
2025-05-08 17:21:50 +1000

    Cool pipeline

--- backend/app/routes/main.py
+++ backend/app/routes/main.py
@@ -1,29 +1,31 @@
-from ml.pipeline.data_prep import preprocess_data
-from ml.pipeline.graph_builder import build_graph
-from ml.pipeline.model_runner import run_model
-from ml.pipeline.postprocessing import postprocess_results
+from ml.build_graph_from_db import build_graph_from_db
 from flask import Blueprint, request, jsonify
+from db.db_manager import get_db
 
 pipeline_bp = Blueprint("pipeline", __name__)
 
 @pipeline_bp.route("/run_pipeline", methods=['POST'])
 def run_pipeline():
-    data = request.get_json()
-    num_classrooms = data.get("num_classrooms")
+    #data = request.get_json()
+    #num_classrooms = data.get("num_classrooms")
 
-    if num_classrooms is None:
-        return jsonify({"error": "num_classrooms not provided"}), 400
+    #if num_classrooms is None:
+    #    return jsonify({"error": "num_classrooms not provided"}), 400
 
     # Step 1: Preprocess
     #df = preprocess_data()
 
     # Step 2: Build graph
-    graph = build_graph(df)
+    db=get_db()
+    graph = build_graph_from_db(db)
 
-    # Step 3: Run model
+
+    ## Step 3: Run model
     results = run_model(graph, num_classrooms)
 
-    # Step 4: Post-process
-    final_output = postprocess_results(results, df)
+    ## Step 4: Post-process
+    #final_output = postprocess_results(results, df)
 
-    return jsonify({"output": final_output})
+    #return jsonify({"output": final_output})
+
+run_pipeline()

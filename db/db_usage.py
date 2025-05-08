a3550757724d3702fdccea5c1e617e535693e7b7
Pratik Karmacharya <pratik3.3k@gmail.com>
2025-05-08 15:26:52 +1000

    Add initial database and machine learning module files; update import paths

--- db/db_usage.py
+++ db/db_usage.py
@@ -1,5 +1,5 @@
-from db_manager import get_db
+from db.db_manager import get_db
 
 db = get_db()
-results = db.fetch_all("SELECT * FROM raw.participants limit 5;")
+results = db.fetch_all("""SELECT participant_id, perc_academic, perc_effort, attendance FROM raw.participants""")
 print(results)
